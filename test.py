0a94ee4c05c208a64cd5158c94343d92856a71e7
Zak <zhickey2001@gmail.com>
2024-09-25 02:11:52 -0400

    Updated in-line comments within each file, as well as tweaked formatting for output.txt results

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+#This script takes input from input.txt, processes those inputs as prompts for the phi3.5 model, and then stores the outputs in the output.txt file
+
 #SDK for Ollama models that allows for communication to local models; based on the Ollama restAPI
 import ollama
 
@@ -8,12 +10,16 @@ input_file = open("input.txt", "r")
 output_file = open("output.txt", "a")
 
 #reads through each line in the input file and feeds the model those lines
+message_num = 0
 for line in input_file:
     prompt = line.strip()  # Remove leading/trailing whitespace
+    
 
     if not prompt or prompt.startswith('#'):  # Skip empty lines and lines starting with '#'
         continue
-
+    
+    print(prompt)
+    message_num += 1
     message = ""
 
     #main point of contact with model where prompt is fed and output is recorded in output.txt
@@ -29,7 +35,8 @@ for line in input_file:
         print(chunk['message']['content'], end='', flush=True)
         message += chunk['message']['content']
 
-    output_file.write(message + '\n')  
+    #writes output in output.txt in format of <prompt#>: <response>
+    output_file.write('\n\n' + str(message_num) + ': ' + message)  
     print('\n')
 
 
