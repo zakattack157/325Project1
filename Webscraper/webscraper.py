e717ae815151769304f1983552b3b2683ec038bb
Zak <zhickey2001@gmail.com>
2024-10-24 04:01:37 -0400

    Working webscraper for steam game reviews, currently houses examples for the FarCry series

--- Webscraper/webscraper.py
+++ Webscraper/webscraper.py
@@ -20,10 +20,22 @@ def get_steam_reviews(app_id, language='english', num_reviews=10):
     else:
         return f"Failed to retrieve reviews. Status Code: {response.status_code}"
 
-app_id = 2344520
-reviews = get_steam_reviews(app_id)
+input_file = open("Webscraper/scraperInputs.txt", "r")
+num = 1
+for line in input_file:
+    
+    app_id = line.strip()
 
-for review in reviews:
-    print(f"Review ID: {review['recommendationid']}")
-    print(f"Review Content: {review['review']}")
-    print("-" * 40)
+    if not app_id or app_id.startswith('#'):
+        continue
+
+    file = open(f"Webscraper/Reviews/FarCry_{num}.txt", "w")
+
+    num = num + 1
+    reviews = get_steam_reviews(app_id)
+
+    for review in reviews:
+        file.write(f"Review ID: {review['recommendationid']}\n")
+        file.write(f"Review Content: {review['review']}\n")
+        file.write("-" * 40 + "\n")
+    file.close()
